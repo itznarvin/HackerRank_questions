a52c25346dd2f13db50c7caf0d02bf3dd7037546
Narvin Parameswaran <narvincoding@gmail.com>
2025-11-29 15:23:00 +0800

    I just completed SolveMeFirst

---
+++ SolveMeFirst.py
@@ -0,0 +1,12 @@
+# int a will be the first value
+# int b will be the second value
+# the sum of a and b 
+
+def solveMeFirst(a,b):
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
